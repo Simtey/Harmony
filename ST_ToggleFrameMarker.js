705fe8ca999bf5ebf7e2e8ca03891af9bf30b5a2
Simtey <simon.thery@gmail.com>
2022-03-26 09:22:20 +0100

    Update ST_ToggleFrameMarker.js

--- ST_ToggleFrameMarker.js
+++ ST_ToggleFrameMarker.js
@@ -1,6 +1,15 @@
-/* Simon Thery - 2021 - ST_ToggleFrameMarker 1.0
+/* Simon Thery - 2022 - ST_ToggleFrameMarker 1.0
 A script to simply add or delete a frame marker
-to be done --> add this line inside the shortcut XML to make it appear in the shortcuts
+
+When the script is added in Harmony --> to add it as a shortcut :
+1. Edit to the files : C:\Program Files (x86)\Toon Boom Animation\Toon Boom Harmony 21 Premium\resources\shortcuts.xml
+2. Go to the end of the file and on a line above the closing tag </shortcuts>
+3. paste the following lines
+<category condition="not scan and not paint and hasTagging" id="ST_scripts" text="Scripts" >
+	<shortcut checkable="false" id="ToggleFrameMarker"  itemParameter="ToggleFrameMarker in ST_ToggleFrameMarker.js"  responder="scriptResponder" slot="onActionExecuteScript(QString)" text="ToggleFrameMarker"  value=""/>
+</category>
+4. save the sortcuts.xml file (You must have the administrator permission)
+5. boot or reboot Harmony --> you can now find the script in Edit --> Keyboard shortcuts
 */
 scene.beginUndoRedoAccum("Create Frame markers");
 //MessageLog.clearLog();
@@ -10,7 +19,7 @@ var selectedFrame = Timeline.firstFrameSel;
 var color;
 var currentFrameMarker = Timeline.getFrameMarker(selectedLayer, selectedFrame);
 
-function ToggleFrameMarker() {
+function ToggleFrameMarker() {		// colors can be changed --> list = Red, Orange, Yellow, Green, Cyan, Blue, Purple, Pink, White, Black
 	if (currentFrameMarker === undefined) { // if no marker --> place a red one
 		color = "Red";
 		placeFM();
