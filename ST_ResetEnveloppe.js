b19b04bea39f2e590c9a476e173e8d5fe20f1c88
Simtey <simon.thery@gmail.com>
2022-03-27 19:45:10 +0200

    Update ST_ResetEnveloppe.js

--- ST_ResetEnveloppe.js
+++ ST_ResetEnveloppe.js
@@ -1,5 +1,16 @@
 /* Simon Thery - 2022 - ST_ResetEnveloppe 1.0
-select just one point of the enveloppe to reset it.
+select just one point of a deformer to reset it.
+
+When the script is added in Harmony --> to add it as a shortcut :
+1. Edit to the files : C:\Program Files (x86)\Toon Boom Animation\Toon Boom Harmony 21 Premium\resources\shortcuts.xml
+2. Go to the end of the file and on a line above the closing tag </shortcuts>
+3. paste the following lines
+<category condition="not scan and not paint and hasTagging" id="ST_scripts" text="Scripts" >
+	<shortcut checkable="false" id="ResetEnveloppe"  itemParameter="ResetEnveloppe in ST_ResetEnveloppe.js"  responder="scriptResponder" slot="onActionExecuteScript(QString)" text="ResetEnveloppe"  value=""/>
+</category>
+4. save the sortcuts.xml file (You must have the administrator permission)
+5. boot or reboot Harmony --> you can now find the script in Edit --> Keyboard shortcuts
+*/
 */
 var selectedNode = selection.selectedNode(selectedNode);
 var parent = node.parentNode(selectedNode);
