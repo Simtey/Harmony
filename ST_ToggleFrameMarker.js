4e1eac5e53c95f713e3e38874bbcecdd9fac8bd8
Simtey <58445254+Simtey@users.noreply.github.com>
2022-03-28 01:21:16 +0200

    Update ST_ToggleFrameMarker.js

--- ST_ToggleFrameMarker.js
+++ ST_ToggleFrameMarker.js
@@ -39,7 +39,6 @@ function ToggleFrameMarker() {		// colors can be changed --> list = Red, Orange,
 
 	function deleteFM() {
 		var markerID = currentFrameMarker.id; // gets the marker ID
-		// var markerColor = currentFrameMarker.type; // gets the marker color
 		Timeline.deleteFrameMarker(selectedLayer, markerID); // delete the marker on the current selected frame
 	}
 }
