13c72dfca4f0ed95b9eb560ce39e4bffffe3390c
Simtey <simon.thery@gmail.com>
2022-04-01 18:06:45 +0200

    Update ST_emptyDrawingPivotInPlace.js
    condition to put back the allLayerIsSelected at its original parameter.

--- ST_emptyDrawingPivotInPlace.js
+++ ST_emptyDrawingPivotInPlace.js
@@ -9,18 +9,23 @@ create an empty drawing which keep the pivot in place and the deformer + select 
 */
 function emptyDrawingPivotInPlace() {
 	scene.beginUndoRedoAccum("Creates a new drawing while keeping the old pivot");
-		var selectedNode = selection.selectedNode(0);
-		var selectedFrame = Timeline.firstFrameSel;
-		var drawingColumn = node.linkedColumn(selectedNode, "DRAWING.ELEMENT");
-			column.duplicateDrawingAt(drawingColumn, selectedFrame);
-			Action.perform("onActionChooseSelectTool()");
-			ToolProperties.setApplyAllArts(true);
-			Action.perform("selectAll()", "cameraView");
-			Action.perform("deleteSelection()", "cameraView");
-			ToolProperties.setApplyAllArts(false);
-			Action.perform("onActionChoosePencilTool()"); // choose the pencil
-			//Action.perform("onActionChooseBrushTool()");  // choose the brush
-			//Action.perform("onActionChooseSpTransformTool()");
-			DrawingTools.setCurrentArt(4); //Underlay(1), Colour Art(2), Line Art(4), Overlay(8)
+	var selectedNode = selection.selectedNode(0);
+	var selectedFrame = Timeline.firstFrameSel;
+	var drawingColumn = node.linkedColumn(selectedNode, "DRAWING.ELEMENT");
+	column.duplicateDrawingAt(drawingColumn, selectedFrame);
+	Action.perform("onActionChooseSelectTool()");
+	var allLayerIsSelected = ToolProperties.getApplyToAllLayers();
+	if (allLayerIsSelected === false) {
+		ToolProperties.setApplyAllArts(true);
+	}
+	Action.perform("selectAll()", "cameraView");
+	Action.perform("deleteSelection()", "cameraView");
+	if (allLayerIsSelected === false) {
+		ToolProperties.setApplyAllArts(false);
+	}
+	Action.perform("onActionChoosePencilTool()"); // choose the pencil
+	//Action.perform("onActionChooseBrushTool()");  // choose the brush
+	//Action.perform("onActionChooseSpTransformTool()"); // put back the transform tool
+	DrawingTools.setCurrentArt(4); //Underlay(1), Colour Art(2), Line Art(4), Overlay(8)
 	scene.endUndoRedoAccum();
 }
