9e576b78d2d4c4fd7628d8ba84736f7e1c8161ff
Simtey <58445254+Simtey@users.noreply.github.com>
2024-03-29 23:03:33 +0100

    Update ST_FillDrawingExposures.js

--- ST_FillDrawingExposures.js
+++ ST_FillDrawingExposures.js
@@ -1,32 +1,32 @@
 // ST_FillDrawingExposures - Simon Thery 2024 - MPL-2.0 - Fill the empty drawing regarding the previous one
-var columnName = Timeline.selToColumn(0);
-var drawExpoName = column.getDrawingName(columnName, frame.current());
-
-if (column.type(columnName) === "DRAWING") {
-	for (var i = frame.current(); i >= 1; i--) { 
-		if (column.getDrawingName(columnName, i) !== drawExpoName) {
-			var previousDrawExpo = i;
-			break;
+function ST_FillDrawingExposures() {
+	var columnName = Timeline.selToColumn(0);
+	var drawExpoName = column.getDrawingName(columnName, frame.current());
+	if (column.type(columnName) === "DRAWING") {
+		for (var i = frame.current(); i >= 1; i--) {
+			if (column.getDrawingName(columnName, i) !== drawExpoName) {
+				var previousDrawExpo = i;
+				break;
+			}
+		}
+		for (var j = frame.current(); j <= frame.numberOf(); j++) {
+			if (column.getDrawingName(columnName, j) !== drawExpoName) {
+				var nextDrawExpoName = column.getDrawingName(columnName, j);
+				var nextDrawExpo = j;
+				break;
+			}
 		}
 	}
-	for (var j = frame.current(); j <= frame.numberOf(); j++) {
-		if (column.getDrawingName(columnName, j) !== drawExpoName) {
-			var nextDrawExpoName = column.getDrawingName(columnName, j);
-			var nextDrawExpo = j;
-			break;
+	scene.beginUndoRedoAccum("ST_FillDrawingExposures");
+	if (previousDrawExpo) {
+		column.fillEmptyCels(columnName, previousDrawExpo, nextDrawExpo);
+	} else if (nextDrawExpoName && !drawExpoName) {
+		var layerName = column.getDisplayName(columnName) + "-";
+		var drawingSubName = nextDrawExpoName.slice(layerName.length, -4)
+		for (var i = 1; i < nextDrawExpo; i++) {
+			column.setEntry(columnName, 1, i, drawingSubName);
 		}
+		column.removeDuplicateKeyDrawingExposureAt(columnName, nextDrawExpo);
 	}
+	scene.endUndoRedoAccum("");
 }
-scene.beginUndoRedoAccum("ST_FillDrawingExposures");
-if (previousDrawExpo) {
-	column.fillEmptyCels(columnName, previousDrawExpo, nextDrawExpo);
-} else if (nextDrawExpoName && !drawExpoName) {
-	var layerName = column.getDisplayName(columnName) + "-";
-	var drawingSubName = nextDrawExpoName.slice(layerName.length, -4)
-	for (var i = 1; i < nextDrawExpo; i++) {
-		column.setEntry(columnName, 1, i, drawingSubName);
-		var duplicatedFrame = i + 1;
-	}
-	column.removeDuplicateKeyDrawingExposureAt(columnName, duplicatedFrame);
-}
-scene.endUndoRedoAccum("");
